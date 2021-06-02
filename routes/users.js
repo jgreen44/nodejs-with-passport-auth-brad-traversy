346d7d3a869937501cd8255dc091ddbd970562e6
jgreen44 <jgreen44@asu.edu>
2021-06-01 20:54:41 -0500

    update

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,48 @@ router.get('/register', (req, res) => {
     res.render('register');
 })
 
+// Register Handle
+router.post("/register", (req, res) => {
+    // console.log(req.body)
+    // res.send('Hello')
+    // array destructuring
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+
+    // Check required fields
+    if(!name || !email || !password || !password2){
+        errors.push(({
+            msg: 'Please fill in all fields'
+        }));
+    }
+
+    // Check passwords match
+    if(password !== password2){
+        errors.push({
+            msg: 'Passwords do not match'
+        });
+    }
+
+    // Check len of < 6 char for passwords
+    if(password.length < 6) {
+        errors.push({
+            msg: 'Password should be at least six characters'
+        });
+    }
+
+    if (errors.length > 0){
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    }else {
+        res.send('pass')
+    }
+
+})
+
 
 module.exports = router;
